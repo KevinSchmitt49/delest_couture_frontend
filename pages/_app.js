033dba09ec3a9d5ee750ad613fb152092301da14
Kévin <k.schmitt.graph@gmail.com>
2025-05-12 13:23:17 +0200

    résolution problème hydratation avec création fichier document

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,6 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Delest Couture</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       <Component {...pageProps} />
     </>
