56a4e8d5281617329838f81354d2e19cc3e3e7c4
Kévin <k.schmitt.graph@gmail.com>
2025-05-16 11:12:29 +0200

    fiche produit desktop + menu déroulant taille, couleurs + quantité

--- components/FicheProduit.js
+++ components/FicheProduit.js
@@ -4,52 +4,91 @@ import { useState } from "react";
 
 function FicheProduit({ product }) {
   const [selectedImage, setSelectedImage] = useState(product.images[0]);
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+  const [selectedQuantity, setSelectedQuantity] = useState(1);
+
+  const decrementQuantity = () => {
+    selectedQuantity > 1 ? setSelectedQuantity(selectedQuantity - 1) : "";
+  };
+  const incrementQuantity = () => {
+    setSelectedQuantity(selectedQuantity + 1);
+  };
 
   if (!product || !product.images) return <p>Produit introuvable</p>;
 
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.gallery}>
-          <div className={styles.mainImage}>
-            <Image
-              src={selectedImage}
-              alt="Image principale"
-              width={400}
-              height={600}
-            />
-          </div>
-          <div className={styles.thumbnails}>
-            {product.images.map((src, index) => (
-              <img
-                key={index}
-                src={src}
-                alt={`Thumbnail ${index}`}
-                onClick={() => setSelectedImage(src)}
-                className={styles.thumb}
-              />
-            ))}
-          </div>
+    <div className={styles.main}>
+      <div className={styles.gallery}>
+        <div className={styles.mainImage}>
+          <Image
+            src={selectedImage}
+            alt="Image principale"
+            width={400}
+            height={600}
+          />
         </div>
+        <div className={styles.thumbnails}>
+          {product.images.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`Thumbnail ${index}`}
+              onClick={() => setSelectedImage(src)}
+              className={styles.thumb}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.container}>
         <h2 className={styles.title}>{product.name}</h2>
         <p className={styles.price}>{product.price}</p>
         <p>Description courte : {product.shortDescription}</p>
         <h2 className={styles.title}>Description détaillée</h2>
         <p>{product.description}</p>
-      </div>
-      {/*-----------sticky add to cart-----------------*/}
-      <div className={styles.addToCartContent}>
-        <div className={styles.selectButton}>
-          <div className={styles.taille}>Taille 38</div>
-          <div className={styles.color}>Blue Dark</div>
-        </div>
-        <div className={styles.quantityAndAddToCart}>
-          <div className={styles.quantity}>
-            <img src="/-.png" alt="-" />
-            2
-            <img src="/+.png" alt="+" />
+        {/*-----------sticky add to cart-----------------*/}
+        <div className={styles.addToCartContent}>
+          <div className={styles.selectButton}>
+            <select
+              className={styles.taille}
+              id="size"
+              value={selectedSize}
+              onChange={(e) => setSelectedSize(e.target.value)}
+            >
+              <option value="">Taille</option>
+              {product.sizes.map((size, index) => (
+                <option key={index} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>
+
+            <select
+              className={styles.color}
+              id="color"
+              value={selectedColor}
+              onChange={(e) => setSelectedColor(e.target.value)}
+            >
+              <option value="">Couleur</option>
+              {product.colors.map((color, index) => (
+                <option key={index} value={color}>
+                  {color}
+                </option>
+              ))}
+            </select>
           </div>
-          <button className={styles.addToCart}>Ajouter au panier</button>
+          <div className={styles.quantityAndAddToCart}>
+            <div className={styles.quantity}>
+              <button onClick={() => decrementQuantity()}>
+                <img src="/-.png" alt="-" />
+              </button>
+              {selectedQuantity}
+              <button onClick={() => incrementQuantity()}>
+                <img src="/+.png" alt="+" />
+              </button>
+            </div>
+            <button className={styles.addToCart}>Ajouter au panier</button>
+          </div>
         </div>
       </div>
     </div>
--- pages/produit/[id].js
+++ pages/produit/[id].js
@@ -18,6 +18,8 @@ const products = [
       "Le confort et la beauté réunis dans un seul modèle. Zip invisible dans la couture coté. Sertie de Trois pierres précieuses Swarovski.",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Dark blue", "pink", "golden"],
   },
   {
     id: 2,
@@ -29,6 +31,8 @@ const products = [
       "Le confort et la beauté réunis dans un seul modèle. Zip invisible dans la couture coté. Sertie de Trois pierres précieuses Swarovski.",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Dark blue", "pink", "golden"],
   },
   {
     id: 3,
@@ -40,6 +44,8 @@ const products = [
       "Le confort et la beauté réunis dans un seul modèle. Zip invisible dans la couture coté. Sertie de Trois pierres précieuses Swarovski.",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Dark blue", "pink", "golden"],
   },
   {
     id: 4,
@@ -55,6 +61,8 @@ const products = [
       "Le confort et la beauté réunis dans un seul modèle. Zip invisible dans la couture coté. Sertie de Trois pierres précieuses Swarovski.",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Dark blue", "pink", "golden"],
   },
 ];
 
